6ad520e525f199352dc35c6d0e5c2c988e598c7e
ashath-treat <ashathkavis@treatooo.org>
2025-12-11 19:17:05 +0530

    Alter content of footer, about and work sections

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -42,11 +42,11 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:hello@micael.com" className="p-text">hello@micael.com</a>
+          <a href="mailto:hello@ashathkavi.com" className="p-text">hello@ashathkavi.com</a>
         </div>
         <div className="app__footer-card">
           <img src={images.mobile} alt="phone" />
-          <a href="tel:+1 (123) 456-7890" className="p-text">+1 (123) 456-7890</a>
+          <a href="tel:+1 (123) 456-7890" className="p-text">+94768571120</a>
         </div>
       </div>
       {!isFormSubmitted ? (
